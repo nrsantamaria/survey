3553c35197060291ff7b13492a178d6bf1290ed6
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-03-24 14:38:29 -0700

    add columns to index.html to seperate questions into 3 columns on the screen.

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,36 @@ $(document).ready(function() {
   $("form#programs").submit(function(event) {
 
     var name = $("input#name").val();
+
     var favactivity = parseInt($("input:radio[name=favactivity]:checked").val());
+
     var style = parseInt($("input:radio[name=style]:checked").val());
+
     var vacay = parseInt($("input:radio[name=vacay]:checked").val());
+
     var hotel = parseInt($("input:radio[name=hotel]:checked").val());
+
     var computer = parseInt($("input:radio[name=computer]:checked").val());
+
     var idol = parseInt($("input:radio[name=idol]:checked").val());
+
     var work = parseInt($("input:radio[name=work]:checked").val());
-    var color = $("input:radio[name=color]:checked").val();
+
+    var results = parseFloat((favactivity + style + vacay + hotel + computer + idol + work) / 7);
 
     $(".programs").hide();
 
-    if (color) {
-      var results = parseFloat((favactivity + style + vacay + hotel + computer + idol + work) / 7);
-      if (results >= 2.85) {
-        $("#c-sharp").show();
-      } else if (results >= 1.71) {
-        $("#php").show();
-      } else {
-        $("#ruby").show();
-      }
+    if (results >= 2.85) {
+      $("#c-sharp").fadeIn('slow');
+    } else if (results >= 1.71) {
+      $("#php").fadeIn('slow');
     } else {
-      $(".radio").addClass("has-error");
+      $("#ruby").fadeIn('slow');
     }
 
     $(".name").empty().append(name);
 
     event.preventDefault();
+
   });
 });
