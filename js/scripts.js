1c1e60fc191596bbcd82b17eb00cd10dfb299824
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-03-24 12:12:32 -0700

    update function on scripts.js to show results based on selections using average of values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   $("form#programs").submit(function(event) {
 
-    var favactivity = parseInt($("select#favactivity").val());
-    var style = parseInt($("select#style").val());
-    var vacay = parseInt($("select#vacay").val());
-    var hotel = parseInt($("select#hotel").val());
-    var computer = parseInt($("select#computer").val());
-    var idol = parseInt($("select#idol").val());
-    var work = parseInt($("select#work").val());
-    var results = (favactivity + style + vacay + hotel + computer + idol + work)
+    var favactivity = parseFloat($("select#favactivity").val());
+    var style = parseFloat($("select#style").val());
+    var vacay = parseFloat($("select#vacay").val());
+    var hotel = parseFloat($("select#hotel").val());
+    var computer = parseFloat($("select#computer").val());
+    var idol = parseFloat($("select#idol").val());
+    var work = parseFloat($("select#work").val());
+    var results = (favactivity + style + vacay + hotel + computer + idol + work) / 7;
 
     $(".programs").hide();
 
-    if (results >= 8) {
-      $("#ruby").show();
-    } else if (results <=6) {
+    if (results >= 3.14) {
       $("#c-sharp").show();
-    } else {
+    } else if (results <= 2.85) {
       $("#php").show();
+    } else if (results <= 1.71){
+      $("#ruby").show();
     }
 
     event.preventDefault();
