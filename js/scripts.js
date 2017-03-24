67ca4a4e6d11aaf90432b396327d8d5d7f407373
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-03-24 13:57:53 -0700

    change input dropdown selector on index.html to radio buttons, update scripts.js to appropriate tags.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
   $("form#programs").submit(function(event) {
+
     var name = $("input#name").val();
-    var favactivity = parseInt($("select#favactivity").val());
-    var style = parseInt($("select#style").val());
-    var vacay = parseInt($("select#vacay").val());
-    var hotel = parseInt($("select#hotel").val());
-    var computer = parseInt($("select#computer").val());
-    var idol = parseInt($("select#idol").val());
-    var work = parseInt($("select#work").val());
-    var results = parseFloat((favactivity + style + vacay + hotel + computer + idol + work) / 7);
+    var favactivity = parseInt($("input:radio[name=favactivity]:checked").val());
+    var style = parseInt($("input:radio[name=style]:checked").val());
+    var vacay = parseInt($("input:radio[name=vacay]:checked").val());
+    var hotel = parseInt($("input:radio[name=hotel]:checked").val());
+    var computer = parseInt($("input:radio[name=computer]:checked").val());
+    var idol = parseInt($("input:radio[name=idol]:checked").val());
+    var work = parseInt($("input:radio[name=work]:checked").val());
+    var color = $("input:radio[name=color]:checked").val();
 
     $(".programs").hide();
 
-    if (results >= 2.85) {
-      $("#c-sharp").show();
-    } else if (results >= 1.71) {
-      $("#php").show();
+    if (color) {
+      var results = parseFloat((favactivity + style + vacay + hotel + computer + idol + work) / 7);
+      if (results >= 2.85) {
+        $("#c-sharp").show();
+      } else if (results >= 1.71) {
+        $("#php").show();
+      } else {
+        $("#ruby").show();
+      }
     } else {
-      $("#ruby").show();
+      $(".radio").addClass("has-error");
     }
-    $(".name").empty().append(name)
+
+    $(".name").empty().append(name);
+
     event.preventDefault();
   });
 });
